4a10d224f6faf386e7f69d0105aecb06a088cbc8
FloatingDream1001 <2011526487@qq.com>
2023-01-24 14:27:07 +0800

    Site updated: 2023-01-24 14:27:06

--- js/commentBarrage.js
+++ js/commentBarrage.js
@@ -1,4 +1,15 @@
+//commentBarrage v3.0 By Ariasaka
+//for swiper and both(pjax or nopjax)
+//ainb?!!!! qwq
+/document.addEventListener('pjax:complete', startbarrage); //不用pjax请注释掉这一行
+document.addEventListener('DOMContentLoaded', startbarrage);
 
+function startbarrage(){
+try{
+clearInterval(timer);
+document.querySelector('.comment-barrage').innerHTML="";
+delete sw;
+}catch(err){}
 const commentBarrageConfig = {
     //浅色模式和深色模式颜色，务必保持一致长度，前面是背景颜色，后面是字体，随机选择，默认这个颜色还好
     lightColors:[
@@ -7,14 +18,13 @@ const commentBarrageConfig = {
     darkColors:[
         ['var(--lyx-black-acrylic2)','var(--lyx-white)'],
     ],
-    //同时最多显示弹幕数
-    maxBarrage: 1,
+    //v3仅支持一个弹幕
     //弹幕显示间隔时间，单位ms
     barrageTime: 3000,
     //twikoo部署地址（Vercel、私有部署），腾讯云的为环境ID
     twikooUrl: "https://blogtwikoo.yangyuezz.top",
     //token获取见前文
-    accessToken: "{YOUR_TOKEN}",
+    accessToken: "{3a7604d10a4e4aa0606ef98dd0393bd5}",
     pageUrl: window.location.pathname,
     barrageTimer: [],
     barrageList: [],
@@ -27,7 +37,18 @@ const commentBarrageConfig = {
     //头像cdn，默认cravatar
     avatarCDN: "cravatar.cn",
 }
-
+function checkURL(URL){
+    var str=URL;
+    //判断URL地址的正则表达式为:http(s)?://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)?
+    //下面的代码中应用了转义字符"\"输出一个字符"/"
+    var Expression=/http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/;
+    var objExp=new RegExp(Expression);
+    if(objExp.test(str)==true){
+    return true;
+    }else{
+    return false;
+    }
+    } 
 function isInViewPortOfOne (el) {
     const viewPortHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight 
     const offsetTop = el.offsetTop
@@ -35,44 +56,54 @@ function isInViewPortOfOne (el) {
     const top = offsetTop - scrollTop
     return top <= viewPortHeight
 }
+if(location.href.indexOf("posts")!=-1||location.href.indexOf("posts")!=-1)
 document.onscroll = function() {
     if(commentBarrageConfig.displayBarrage){
-    if(isInViewPortOfOne(document.getElementById("post-comment"))){
-        document.getElementsByClassName("comment-barrage")[0].setAttribute("style",`display:none;`)
+        if(isInViewPortOfOne(document.getElementById("post-comment"))){
+            $(".comment-barrage").hide();
+        }
+        else{
+            $(".comment-barrage").show();
+        }
     }
-    else{
-        document.getElementsByClassName("comment-barrage")[0].setAttribute("style","")
-    }
-}
   }
 function initCommentBarrage(){
-        var data = JSON.stringify({
-          "event": "COMMENT_GET",
-          "commentBarrageConfig.accessToken": commentBarrageConfig.accessToken,
-          "url": commentBarrageConfig.pageUrl
-        });
-        var xhr = new XMLHttpRequest();
-        xhr.withCredentials = true;
-        xhr.addEventListener("readystatechange", function() {
-          if(this.readyState === 4) {
+    
+    var data = JSON.stringify({
+        "event": "COMMENT_GET",
+        "commentBarrageConfig.accessToken": commentBarrageConfig.accessToken,
+        "url": commentBarrageConfig.pageUrl
+    });
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+    xhr.addEventListener("readystatechange", function() {
+        if(this.readyState === 4) {
             commentBarrageConfig.barrageList = commentLinkFilter(JSON.parse(this.responseText).data);
             commentBarrageConfig.dom.innerHTML = '';
-          }
-        });
-        xhr.open("POST", commentBarrageConfig.twikooUrl);
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.send(data);
-        setInterval(()=>{
-            if(commentBarrageConfig.barrageList.length){
+            for(commentBarrageConfig.barrageIndex=0;commentBarrageConfig.barrageIndex<commentBarrageConfig.barrageList.length;commentBarrageConfig.barrageIndex++){
                 popCommentBarrage(commentBarrageConfig.barrageList[commentBarrageConfig.barrageIndex]);
-                commentBarrageConfig.barrageIndex += 1;
-                commentBarrageConfig.barrageIndex %= commentBarrageConfig.barrageList.length;
             }
-            if(commentBarrageConfig.barrageTimer.length > (commentBarrageConfig.barrageList.length > commentBarrageConfig.maxBarrage?commentBarrageConfig.maxBarrage:commentBarrageConfig.barrageList.length)){
-                removeCommentBarrage(commentBarrageConfig.barrageTimer.shift())
+            kkksc03=new Swiper('.barrageswiper', {
+                direction: "vertical",
+                loop: true,
+                mousewheel:true,
+                autoplay: {
+                    delay: commentBarrageConfig.barrageTime,
+                    stopOnLastSlide: false,
+                    disableOnInteraction: false,
+                }, 
+            })
+            kkksc03.el.onmouseover = function(){
+                kkksc03.autoplay.stop();
             }
-        },commentBarrageConfig.barrageTime)
-
+            kkksc03.el.onmouseout = function(){
+                kkksc03.autoplay.start();
+            }
+        }
+    });
+    xhr.open("POST", commentBarrageConfig.twikooUrl);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(data);
 }
 function commentLinkFilter(data){
     data.sort((a,b)=>{
@@ -102,28 +133,62 @@ function popCommentBarrage(data){
     barrage.className = 'comment-barrage-item'
     let ran = Math.floor(Math.random()*commentBarrageConfig.lightColors.length)
     document.getElementById("barragesColor").innerHTML=`[data-theme='light'] .comment-barrage-item { background-color:${commentBarrageConfig.lightColors[ran][0]};color:${commentBarrageConfig.lightColors[ran][1]}}[data-theme='dark'] .comment-barrage-item{ background-color:${commentBarrageConfig.darkColors[ran][0]};color:${commentBarrageConfig.darkColors[ran][1]}}`;
-
-    barrage.innerHTML = `
-        <div class="barrageHead">
-            <img class="barrageAvatar" src="https://${commentBarrageConfig.avatarCDN}/avatar/${data.mailMd5}?d=${commentBarrageConfig.noAvatarType}"/>
-            <div class="barrageNick">${data.nick}</div>
-            <a href="javascript:switchCommentBarrage()" style="font-size:20px">×</a>
-        </div>
-        <div class="barrageContent">${data.comment}</div>
-    `
-    commentBarrageConfig.barrageTimer.push(barrage);
-    commentBarrageConfig.dom.append(barrage);
-}
-function removeCommentBarrage(barrage){
-    barrage.className = 'comment-barrage-item out';
-
-    if(commentBarrageConfig.maxBarrage!=1){
-        setTimeout(()=>{
-            commentBarrageConfig.dom.removeChild(barrage);
-        },1000)
-    }else{
-        commentBarrageConfig.dom.removeChild(barrage);
+    if(data.avatar!=undefined){
+        if(data.link!=undefined){
+            if(!checkURL(data.link)){
+                data.link="http://"+data.link;
+            }
+            barrage.innerHTML = `
+            <div class="barrageHead">
+                <img class="barrageAvatar" src="${data.avatar}"/>
+                <a href="${data.link}" class="barrageNick" target="_blank">${data.nick}</a>
+                <a href="javascript:switchCommentBarrage()" style="font-size:20px">×</a>
+            </div>
+            `
+        }
+        else{
+            barrage.innerHTML = `
+            <div class="barrageHead">
+                <img class="barrageAvatar" src="${data.avatar}"/>
+                <div class="barrageNick">${data.nick}</div>
+                <a href="javascript:switchCommentBarrage()" style="font-size:20px">×</a>
+            </div>
+            `
+        }
     }
+    else{
+        if(data.link!=undefined){
+            if(!checkURL(data.link)){
+                data.link="http://"+data.link;
+            }
+            barrage.innerHTML = `
+            <div class="barrageHead">
+                <img class="barrageAvatar" src="https://${commentBarrageConfig.avatarCDN}/avatar/${data.mailMd5}?d=${commentBarrageConfig.noAvatarType}"/>
+                <a href="${data.link}" class="barrageNick" target="_blank">${data.nick}</a>
+                <a href="javascript:switchCommentBarrage()" style="font-size:20px">×</a>
+            </div>
+            `
+        }
+        else{
+            barrage.innerHTML = `
+            <div class="barrageHead">
+                <img class="barrageAvatar" src="https://${commentBarrageConfig.avatarCDN}/avatar/${data.mailMd5}?d=${commentBarrageConfig.noAvatarType}"/>
+                <div class="barrageNick">${data.nick}</div>
+                <a href="javascript:switchCommentBarrage()" style="font-size:20px">×</a>
+            </div>
+            `
+        }
+    }
+    barrageContent=document.createElement('a');
+    barrageContent.className="barrageContent";
+    barrageContent.href="#"+data.id;
+    barrageContent.innerHTML=data.comment;
+    barrage.appendChild(barrageContent);
+    aswiper = document.createElement('div');
+    aswiper.className="swiper-slide";
+    aswiper.setAttribute("style","height: 150px;");
+    aswiper.append(barrage);
+    commentBarrageConfig.dom.append(aswiper);
 }
 switchCommentBarrage = function () {
     localStorage.setItem("isBarrageToggle",Number(!Number(localStorage.getItem("isBarrageToggle"))))
@@ -134,21 +199,8 @@ switchCommentBarrage = function () {
         $(commentBarrage).fadeToggle()
     }
 }
+
 }
-$(".comment-barrage").hover(function(){
-    clearInterval(timer);
-},function () {
-    timer=setInterval(()=>{
-        if(commentBarrageConfig.barrageList.length){
-            popCommentBarrage(commentBarrageConfig.barrageList[commentBarrageConfig.barrageIndex]);
-            commentBarrageConfig.barrageIndex += 1;
-            commentBarrageConfig.barrageIndex %= commentBarrageConfig.barrageList.length;
-        }
-        if(commentBarrageConfig.barrageTimer.length > (commentBarrageConfig.barrageList.length > commentBarrageConfig.maxBarrage?commentBarrageConfig.maxBarrage:commentBarrageConfig.barrageList.length)){
-            removeCommentBarrage(commentBarrageConfig.barrageTimer.shift())
-        }
-    },commentBarrageConfig.barrageTime)
-})
 if(localStorage.getItem("isBarrageToggle")==undefined){
     localStorage.setItem("isBarrageToggle","0");
 }else if(localStorage.getItem("isBarrageToggle")=="1"){
@@ -156,5 +208,5 @@ if(localStorage.getItem("isBarrageToggle")==undefined){
     switchCommentBarrage()
 }
 initCommentBarrage()
-
+}
 
